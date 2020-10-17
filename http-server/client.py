2d0e217c6a643bc623d7a1ccaf08312c58e00ab4
Gihyeon-Kwon <rlg1133@naver.com>
2020-10-17 13:20:12 +0900

    asd

---
+++ http-server/client.py
@@ -0,0 +1,51 @@
+from socket import *
+from select import *
+import sys
+from time import ctime
+
+import RPi.GPIO as GPIO
+import time
+
+
+DO = 26
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DO, GPIO.IN)
+
+
+HOST = '127.0.0.1'
+PORT = 10000
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+
+url = 'http://127.0.0.1:3000/dists'
+
+
+try:
+    while True:
+        if GPIO.input(DO) < 0.01:
+            data = {'mst': 'good'}
+            response = web_request(method_name='POST', url=url, dict_data=data)
+            print(response)
+            if response['ok'] == True:
+            	print(response['text'])
+            else:
+                print("fail1")
+
+        else:
+            data = {'mst': 'bad'}
+            response = web_request(method_name='POST', url=url, dict_data=data)
+            print(response)
+            if response['ok'] == True:
+            	print(response['text'])
+            else:
+                print("fail2")
+
+        time.sleep(1)
+
+
+except Exception as e:
+    print('%s:%s' % ADDR)
+    sys.exit()
+
+print('connect is success')
